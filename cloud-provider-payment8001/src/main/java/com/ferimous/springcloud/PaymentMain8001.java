df96a3ecc06dc50f06fb498b6a2819fb3754da6b
郭鹏举 <guopengju@ferimbp.local>
2020-09-30 00:39:16 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/ferimous/springcloud/PaymentMain8001.java
@@ -0,0 +1,10 @@
+package com.ferimous.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringBootApplication.run(.class, args);
+    }
+}
